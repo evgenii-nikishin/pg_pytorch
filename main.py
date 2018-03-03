731d06e0d09a91e0f9ac980a73ef6c6354abd881
Evgenii Nikishin <nikishin.evg@gmail.com>
2018-03-03 14:38:38 +0300

    changed n_episodes to n_timesteps

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from src.agent import AgentA2C
 from src.utils import TrajStats, set_seeds
 
 
-def learn(agent, env, n_episodes=20000, gamma=0.99, lambda_gae=0.95, entr_coef=1e-3, verbose=0):
+def learn(agent, env, n_timesteps=1e5, gamma=0.99, lambda_gae=0.95, entr_coef=1e-3, log_interval=1e4):
     """
     Optimize networks parameters via interacting with env
     Arguments:
@@ -19,17 +19,20 @@ def learn(agent, env, n_episodes=20000, gamma=0.99, lambda_gae=0.95, entr_coef=1
         n_episodes      --  number of full interaction emulations, int
         lambda_gae      --  mixing coefficient in generalized advantage estimation
         entr_coef       --  entropy loss multiplier, float
-        verbose         --  number of episodes to print debug info, int (default is 0: don't print)
+        log_interval    --  number of timesteps to print debug info, int
     """
 
     agent.net.train()
-
     returns = []
-    for e in range(n_episodes):
+    timestep = 0
+    episode = 0
+
+    while timestep < n_timesteps:
         s = env.reset()
         done = False
         ts = TrajStats()
         
+        episode += 1
         while not done:
             logits, value = agent.forward(s)
             a = agent.sample_action(logits)
@@ -37,7 +40,10 @@ def learn(agent, env, n_episodes=20000, gamma=0.99, lambda_gae=0.95, entr_coef=1
             
             ts.append(r, F.log_softmax(logits, dim=-1)[a], value)
             s = s_new
-
+            timestep += 1
+            if timestep % log_interval == 0:
+                print('{} timesteps, av. return: {:.3f}'.format(timestep, np.mean(returns[-50:])))
+        
         advantages = ts.calc_gaes(gamma, lambda_gae)
         episode_returns = ts.calc_episode_returns(gamma)
         logs_pi = torch.cat(ts.logs_pi_a)
@@ -55,24 +61,21 @@ def learn(agent, env, n_episodes=20000, gamma=0.99, lambda_gae=0.95, entr_coef=1
         loss.backward()
         agent.optimizer.step()
 
-        if verbose > 0 and (e+1) % verbose == 0:
-            print('{} episodes, av. return: {:.3f}'.format(e+1, np.mean(returns[-50:])))
-
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--env', default='FrozenLake-v0')
     parser.add_argument('--seed', type=int, default=417)
-    parser.add_argument('--n-episodes', type=int, default=20000)
+    parser.add_argument('--n-timesteps', type=int, default=1e5)
     parser.add_argument('--gamma', type=float, default=0.99)
-    parser.add_argument('--verbose', type=int, default=1000)
+    parser.add_argument('--log_interval', type=int, default=1e4)
     args = parser.parse_args()
 
     env = gym.make(args.env)
     agent = AgentA2C(env)
 
     set_seeds(env, args.seed)
-    learn(agent, env, n_episodes=args.n_episodes, gamma=args.gamma, verbose=args.verbose)
+    learn(agent, env, n_timesteps=args.n_timesteps, gamma=args.gamma, log_interval=args.log_interval)
 
 
 if __name__ == '__main__':
